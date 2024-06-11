6a8a08d9fce61af6ecb365f45efcb39a69638474
Mutim <taormino24@gmail.com>
2024-06-11 12:22:09 -0500

    Update main.py
    Added compatibility for linux systems. This should have been done in the first place.

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
 import time
-from concurrent.futures import ThreadPoolExecutor
 import os
 import itertools
 import shutil
-
+from concurrent.futures import ThreadPoolExecutor
 from PIL import Image
 
 
-skin_path = r'.\lib\skins'
-
-
-def zip_and_send():
+def zip_and_send(finals_path):
     try:
         print(f'\nZipping files. Please Wait....')
-        shutil.make_archive('complete_skins', 'zip', r'.\finals')
+        shutil.make_archive('complete_skins', 'zip', finals_path)
         print(f'\nSuccessfully zipped final images.')
-    # Send to discord
     except Exception as err:
         print(f'An error has occurred while zipping: {err}')
 
 
-def get_images() -> dict:
+def get_images(skin_path):
     image_dict = {}
-
     for part in os.listdir(skin_path):
         skins = os.path.join(skin_path, part)
         if os.path.isdir(skins):
-            path = os.path.normpath(skins)
-            image_dict[path.split(os.sep)[-1]] = os.listdir(skins)
-
+            image_dict[part] = os.listdir(skins)
     return image_dict
 
 
-def data_check():
+def data_check(skin_path, finals_path):
     print('Performing Data Check on Files...')
-
     # Create /finals directory if not exists
-    if not os.path.exists(r'.\finals'):
-        os.makedirs(r'.\finals')
-
+    if not os.path.exists(finals_path):
+        print(f'Finals Directory ( {finals_path} ) not found! Generating directory now....')
+        os.makedirs(finals_path)
     # Verify that the parts directories are not empty
     for part in os.listdir(skin_path):
-        if not len(os.listdir(f'{skin_path}\\{part}')) > 0:
-            exit(fr'{skin_path}\{part} directory is empty!')
+        part_path = os.path.join(skin_path, part)
+        if not os.listdir(part_path):
+            exit(f'{part_path} directory is empty!')
 
 
-def image_iterator(portions):
+def image_iterator(skin_path, finals_path, portions):
     tasks = []
     combinations = itertools.product(
         portions['base_skin'],
@@ -53,31 +45,26 @@ def image_iterator(portions):
         portions['eyes'],
         portions['hair']
     )
-
     for b, x, x1, x2 in combinations:
-        file_name = fr'.\final\{b[:-4]}_{x[:-4]}_{x1[:-4]}_{x2[:-4]}.png'
+        file_name = os.path.join(finals_path, f"{b[:-4]}_{x[:-4]}_{x1[:-4]}_{x2[:-4]}.png")
         if not os.path.exists(file_name):
             tasks.append((b, x, x1, x2))
         else:
             print(f'Skipping {b[:-4]}, {x[:-4]}, {x1[:-4]}, {x2[:-4]}')
-
     with ThreadPoolExecutor() as executor:
-        futures = [executor.submit(build_skin, *task) for task in tasks]
+        futures = [executor.submit(build_skin, skin_path, finals_path, *task) for task in tasks]
         for future in futures:
             future.result()
 
 
-def build_skin(b, x, x1, x2):
+def build_skin(skin_path, finals_path, b, x, x1, x2):
     try:
-        base_path = r'.\lib\skins'
-        final_path = r'.\finals'
         blank_image = Image.new('RGBA', (64, 64), (255, 255, 255, 0))
-
         # Part layers
-        base_layer_image = Image.open(fr'{base_path}\base_skin\{b}').convert('RGBA')
-        under_image = Image.open(fr'{base_path}\under\{x}').convert('RGBA')
-        eyes_image = Image.open(fr'{base_path}\eyes\{x1}').convert('RGBA')
-        hair_image = Image.open(fr'{base_path}\hair\{x2}').convert('RGBA')
+        base_layer_image = Image.open(os.path.join(skin_path, 'base_skin', b)).convert('RGBA')
+        under_image = Image.open(os.path.join(skin_path, 'under', x)).convert('RGBA')
+        eyes_image = Image.open(os.path.join(skin_path, 'eyes', x1)).convert('RGBA')
+        hair_image = Image.open(os.path.join(skin_path, 'hair', x2)).convert('RGBA')
 
         # Stitch Images
         initial_image = Image.alpha_composite(blank_image, base_layer_image)
@@ -85,21 +72,23 @@ def build_skin(b, x, x1, x2):
         initial_image = Image.alpha_composite(initial_image, eyes_image)
         final_image = Image.alpha_composite(initial_image, hair_image)
 
-        final_image.save(fr'{final_path}\{b[:-4]}_{x[:-4]}_{x1[:-4]}_{x2[:-4]}.png')
-        print(f'Saved: {final_path}\\{b[:-4]}_{x[:-4]}_{x1[:-4]}_{x2[:-4]}.png')
-
+        # Save the final image
+        file_name = f"{b[:-4]}_{x[:-4]}_{x1[:-4]}_{x2[:-4]}.png"
+        final_image.save(os.path.join(finals_path, file_name))
+        print(f'Saved: {os.path.join(finals_path, file_name)}')
     except Exception as e:
         print(f'Error: {e}')
 
 
 if __name__ == '__main__':
     t1 = time.time()
-    
-    data_check()
-    images = get_images()
-    image_iterator(images)
-    zip_and_send()
+    main_directory = os.getcwd()
+    skin_directory = os.path.join(main_directory, 'lib', 'skins')
+    finals_directory = os.path.join(main_directory, 'finals')
 
+    data_check(skin_directory, finals_directory)
+    images = get_images(skin_directory)
+    image_iterator(skin_directory, finals_directory, images)
+    zip_and_send(finals_directory)
     length = time.time() - t1
     print(f'Finished! -- Operation took {int(length // 60)}:{int(length % 60):02} minutes')
-
